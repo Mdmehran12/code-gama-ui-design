b64d8690d1fc78f287acae0c9deb790ae83e2fd0
Raiyan Ahmed <ray38250968@gmail.com>
2025-02-22 16:36:09 +0000

    Fixed netflix logo not showing

--- src/Components/CompaniesSlide.jsx
+++ src/Components/CompaniesSlide.jsx
@@ -1,7 +1,7 @@
 import Netflix from '../assets/images/Item-5.png';
 import Disney from '../assets/images/Item4.png';
-import Airbnb from '../assets/images/item-2.png';
-import Microsoft from '../assets/images/item-3.png';
+import Airbnb from '../assets/images/Item-2.png';
+import Microsoft from '../assets/images/Item-3.png';
 import Duolingo from '../assets/images/Item-1.png';
 import dashboardimg from "../assets/images/main-dashboard.png"
 
@@ -13,7 +13,7 @@ const CompaniesSlide = () => {
     const companies = [
         {
             name: "Netflix",
-            img: Netflix
+            logo: Netflix
         },
         {
             name: "Disney",
